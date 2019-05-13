a724d8f4b6689b9d4de6fc16192b39e5e14630ae
pieter-jan <pieter-jan@outlook.com>
2019-05-13 21:01:30 +0200

    les2 oef1 af

--- les 2/oef1/src/java/fact/it/www/controller/OpleidingController.java
+++ les 2/oef1/src/java/fact/it/www/controller/OpleidingController.java
@@ -6,9 +6,11 @@
 package fact.it.www.controller;
 
 import fact.it.www.dao.OpleidingFacade;
+import fact.it.www.entity.Opleiding;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
 import javax.ejb.EJB;
 
 /**
@@ -27,6 +29,42 @@ public class OpleidingController implements Serializable {
      * Creates a new instance of OpleidingController
      */
     public OpleidingController() {
+    }
+
+    public Opleiding getOpleiding() {
+        return opleiding;
+    }
+
+    public void setOpleiding(Opleiding opleiding) {
+        this.opleiding = opleiding;
+    }
+    
+    public List<Opleiding> findAll() {
+        return this.opleidingFacade.findAll();
+    }
+    
+    public String add1() {
+        this.opleiding = new Opleiding();
+        return "add";
+    }
+    
+    public String add2() {
+        this.opleidingFacade.create(this.opleiding);
+        return "admin";
+    }
+    
+    public String edit1(Opleiding o) {
+        this.opleiding = o;
+        return "edit";
+    }
+    
+    public String edit2() {
+        this.opleidingFacade.edit(this.opleiding);
+        return "admin";
+    }
+    
+    public void delete(Opleiding o) {
+        this.opleidingFacade.remove(o);
     }
     
 }
